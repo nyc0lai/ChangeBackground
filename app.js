9344d65fdad39b7cab910b09edbe58aeec883012
nyc0lai <nyc0lai@gmail.com>
2022-03-22 16:19:35 +0200

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 
 const colors = ['yellow', 'red', 'green', 'blue', 'black', 'orange', 'pink'];
 
-let btn = document.getElementById('click');
-
-btn.addEventListener('click', changeColor)
+document.getElementById('btn').addEventListener('click', changeColor)
 
 function changeColor() {
-    let color = colors[getRndInteger(0, colors.length)];
+    let color = colors[Math.floor(Math.random()*colors.length)];
     document.body.style.backgroundColor = color;
 
 }
-
-function getRndInteger(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
